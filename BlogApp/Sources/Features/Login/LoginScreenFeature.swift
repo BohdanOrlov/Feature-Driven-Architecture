5dde45e85d3ba3aec7b38d87eb9742b10b13cf87
Bohdan Orlov <bohdan.orlov@gmail.com>
2018-04-02 19:49:32 +0100

    Extracted LoginViewController from LoginScreenFeature.

--- BlogApp/Sources/Features/Login/LoginScreenFeature.swift
+++ BlogApp/Sources/Features/Login/LoginScreenFeature.swift
@@ -7,114 +7,7 @@ import Core
 import UIKit
 import Domain
 
-
-protocol LoginViewDataRendering: class {
-    var showsActivityIndicator: Bool { get set }
-    var didTapButton: ((SessionCredentials) -> Void)? {get set}
-}
-
-typealias LoginViewControlling = LoginViewDataRendering & UIViewController
-
-struct SessionCredentials {
-    let username: String
-    let password: String
-}
-
-class LoginViewController: UIViewController, LoginViewDataRendering {
-    public var showsActivityIndicator: Bool {
-        get {
-            return self.activityIndicator.isAnimating
-        }
-        set {
-            if newValue {
-                self.activityIndicator.startAnimating()
-            } else {
-                self.activityIndicator.stopAnimating()
-            }
-        }
-    }
-    var didTapButton: ((SessionCredentials) -> Void)?
-    
-    override func viewDidLoad() {
-        self.view.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(self.stackView)
-        self.stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
-        self.stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        self.stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.view.backgroundColor = UIColor.white
-        self.view.addSubview(self.activityIndicator)
-        self.button.rightAnchor.constraint(equalTo: self.activityIndicator.leftAnchor, constant: 40).isActive = true
-        self.button.centerYAnchor.constraint(equalTo: self.activityIndicator.centerYAnchor).isActive = true
-        
-    }
-    
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.spacing = 20
-        stackView.addArrangedSubview(self.usernameField)
-        stackView.addArrangedSubview(self.passwordField)
-        stackView.addArrangedSubview(self.button)
-        return stackView
-    }()
-    
-    private lazy var usernameField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Username (Samantha)"
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.borderStyle = .line
-        return textField
-    }()
-    
-    private lazy var passwordField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Password"
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.borderStyle = .line
-        return textField
-    }()
-    
-    private lazy var button: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Login", for: .normal)
-        button.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
-        return button
-    }()
-    
-    private lazy var activityIndicator: UIActivityIndicatorView = {
-        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-        indicator.translatesAutoresizingMaskIntoConstraints = false
-        return indicator
-    }()
-    
-    @objc
-    private func onButtonTap() {
-        self.didTapButton?(SessionCredentials(username: self.usernameField.text ?? "", password: self.passwordField.text ?? ""))
-    }
-}
-
-
-
-protocol LoginScreenInteracting {
-    
-}
-
-protocol YellowDataViewDataSource {
-}
-
-protocol YellowDataViewDelegate {
-
-}
-
-typealias YellowDataViewAdapting = YellowDataViewDataSource & YellowDataViewDelegate
-
-class YellowDataViewAdapter: YellowDataViewAdapting {
-}
-
-class LoginScreenFeature: LoginScreenInteracting {
+class LoginScreenFeature {
     private weak var loginViewController: LoginViewControlling?
     private let viewControllerPresenter: ViewControllerPresenting
     private let sessionService: SessionServiceProtocol
@@ -147,10 +40,10 @@ class LoginScreenFeature: LoginScreenInteracting {
             fallthrough
         case .failed(_): fallthrough
         case .stopped:
-            loginViewController.view.isUserInteractionEnabled = true
+            loginViewController.isUserInteractionEnabled = true
             loginViewController.showsActivityIndicator = false
         case .starting:
-            loginViewController.view.isUserInteractionEnabled = false
+            loginViewController.isUserInteractionEnabled = false
             loginViewController.showsActivityIndicator = true
         case .started(let session):
             self.didLogin(session)
---
+++ BlogApp/Sources/Features/Login/LoginViewController.swift
@@ -0,0 +1,94 @@
+//
+//  LoginViewController.swift
+//  BlogApp
+//
+//  Created by Bohdan Orlov on 02/04/2018.
+//  Copyright © 2018 Bohdan Orlov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Layoutless
+
+struct SessionCredentials {
+    let username: String
+    let password: String
+}
+
+protocol LoginRendering: class {
+    var isUserInteractionEnabled: Bool { get set }
+    var showsActivityIndicator: Bool { get set }
+    var didTapButton: ((SessionCredentials) -> Void)? { get set }
+}
+
+typealias LoginViewControlling = LoginRendering & UIViewController
+
+class LoginViewController: ViewController, LoginRendering {
+    
+    var isUserInteractionEnabled: Bool {
+        get {
+            return view.isUserInteractionEnabled
+        }
+        set {
+            view.isUserInteractionEnabled = newValue
+        }
+    }
+    
+    var showsActivityIndicator: Bool {
+        get {
+            return activityIndicator.isAnimating
+        }
+        set {
+            if newValue {
+                activityIndicator.startAnimating()
+            } else {
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    var didTapButton: ((SessionCredentials) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.activityIndicator)
+        self.button.rightAnchor.constraint(equalTo: self.activityIndicator.leftAnchor, constant: 40).isActive = true
+        self.button.centerYAnchor.constraint(equalTo: self.activityIndicator.centerYAnchor).isActive = true
+        
+    }
+    
+    override var subviewsLayout: AnyLayout {
+        return stack(.vertical, spacing: 20, alignment: .fill)(
+            usernameField,
+            passwordField,
+            button
+        ).centeringInParent().sizing(toWidth: 200)
+    }
+    
+    private lazy var usernameField = UITextField()
+        .set(\.placeholder, "Username (Samantha)")
+        .set(\.borderStyle, .line)
+    
+    private let passwordField = UITextField()
+        .set(\.placeholder, "Password")
+        .set(\.borderStyle, .line)
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        return indicator
+    }()
+    
+    @objc
+    private func onButtonTap() {
+        self.didTapButton?(SessionCredentials(username: self.usernameField.text ?? "", password: self.passwordField.text ?? ""))
+    }
+}
